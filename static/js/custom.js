6f03d6ef900a85a1cc959caa803720b9d3306591
Adam Hałdaś <adam.haldas@lh.pl>
2024-02-11 15:53:54 +0100

    add logo

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,31 +1,31 @@
-// Deklaracje zmiennych
-var themeToggleBtn = document.querySelector("#theme-toggle");
-const element = document.querySelector(".profile_inner");
-const heroImg = document.createElement("img");
-element.appendChild(heroImg);
+// // Deklaracje zmiennych
+// var themeToggleBtn = document.querySelector("#theme-toggle");
+// const element = document.querySelector(".profile_inner");
+// const heroImg = document.createElement("img");
+// element.appendChild(heroImg);
 
 
 
-// Nadanie atrybutów dla obrazka kontaktu
+// // Nadanie atrybutów dla obrazka kontaktu
 
 
-heroImg.classList.add("hero-img");
+// heroImg.classList.add("hero-img");
 
-if (document.body.className.includes("dark")) {
-  heroImg.src = "/img/dark.png";
+// if (document.body.className.includes("dark")) {
+//   heroImg.src = "/img/dark.png";
 
-} else {
-  heroImg.src = "/img/light.png";
-}
+// } else {
+//   heroImg.src = "/img/light.png";
+// }
 
-function changeHeroImg() {
-  if (document.body.className.includes("dark")) {
-    heroImg.src = "/img/dark.png";
-  } else {
-    heroImg.src = "/img/light.png";
-  }
-}
+// function changeHeroImg() {
+//   if (document.body.className.includes("dark")) {
+//     heroImg.src = "/img/dark.png";
+//   } else {
+//     heroImg.src = "/img/light.png";
+//   }
+// }
 
-themeToggleBtn.addEventListener("click", function () {
-  changeHeroImg();
-});
+// themeToggleBtn.addEventListener("click", function () {
+//   changeHeroImg();
+// });
