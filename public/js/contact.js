70d1b50ea2982f9770dd88e50bf72ac46b3f21d2
Adam Hałdaś <adam@MacBook-Pro.local>
2024-04-01 21:21:00 +0200

    hide hamburger menu on a mobile

--- public/js/contact.js
+++ public/js/contact.js
@@ -3,10 +3,6 @@ var themeToggleBtn1 = document.querySelector("#theme-toggle");
 
 var contactImg = document.querySelector(".contact-monkey");
 
-const hamburger = document.querySelector('.hamburger');
-const navigation = document.querySelector('.navigation__list');
-let listItems = document.querySelectorAll('a.navigation__item');
-const nav = document.querySelector('nav');
 // Nadanie atrybutów dla obrazka kontaktu
 
 
@@ -35,26 +31,4 @@ function changeContactImg() {
 
 themeToggleBtn1.addEventListener("click", function () {
     changeContactImg();
-});
-
-
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('hamburger--active');
-  navigation.classList.toggle('navigation--active');
-});
-
-document.querySelectorAll('.navigation__item').forEach((item) => {
-  item.addEventListener('click', () => {
-    hamburger.classList.toggle('hamburger--active');
-    navigation.classList.toggle('navigation--active');
-  });
-});
-
-window.window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) {
-    nav.classList.add('sticky');
-  } else {
-    nav.classList.remove('sticky');
-  }
 });
