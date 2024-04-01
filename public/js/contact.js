7dcef60cc12366056a9ca71b76ce3ff963fede24
Adam Hałdaś <adam@MacBook-Pro.local>
2024-04-01 21:12:29 +0200

    restore previous version with working themes

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,58 +1,60 @@
-// // Deklaracje zmiennych
-// var themeToggleBtn1 = document.querySelector("#theme-toggle");
-// var contactImg = document.querySelector(".contact-monkey");
-// var nav = document.getElementById('menu');
-// var hamburger = document.querySelector('.hamburger');
-// var navigation = document.querySelector('.navigation__list');
+// Deklaracje zmiennych
+var themeToggleBtn1 = document.querySelector("#theme-toggle");
 
-// // Funkcje odpowiadające za obrazki małp i ich kolor.
-// function changeContactImg() {
-//     if (document.body.className.includes("dark")) {
-//         contactImg.src = "/img/contact-dark.png";
-//         contactImg.alt = "contact monkey";
-//         contactImg.className = "contact-monkey";
-//     } else {
-//         contactImg.src = "/img/contact-light.png";
-//         contactImg.alt = "contact monkey";
-//         contactImg.className = "contact-monkey";
-//     }
-// }
+var contactImg = document.querySelector(".contact-monkey");
 
-// // Funkcja obsługująca przesuwanie menu
-// function toggleMenu() {
-//     hamburger.classList.toggle('hamburger--active');
-//     if (navigation) {
-//         navigation.classList.toggle('navigation--active');
-//     }
-//     if (nav) {
-//         nav.classList.toggle('menu--active');
-//     }
-// }
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation__list');
+let listItems = document.querySelectorAll('a.navigation__item');
+const nav = document.querySelector('nav');
+// Nadanie atrybutów dla obrazka kontaktu
 
-// // Nasłuchiwanie kliknięcia na przycisk hamburgera
-// if (hamburger) {
-//     hamburger.addEventListener('click', toggleMenu);
-// }
 
-// // Obsługa przesuwania menu w momencie scrollowania
-// window.addEventListener('scroll', () => {
-//     if (window.scrollY > 0 && nav) {
-//         nav.classList.add('sticky');
-//     } else if (nav) {
-//         nav.classList.remove('sticky');
-//     }
-// });
 
-// // Nasłuchiwanie kliknięcia na elementy menu (aby schować menu po wybraniu opcji)
-// document.querySelectorAll('.navigation__item').forEach((item) => {
-//     item.addEventListener('click', () => {
-//         toggleMenu();
-//     });
-// });
+if (document.body.className.includes("dark")) {
+  contactImg.src = "/img/contact-dark.png";
+  contactImg.alt = "contact monkey";
+  contactImg.className = "contact-monkey";
+} else {
+  contactImg.src = "/img/contact-light.png";
+  contactImg.alt = "contact monkey";
+  contactImg.className = "contact-monkey";
+}
 
-// // Nasłuchiwanie kliknięcia na przycisk zmiany motywu
-// if (themeToggleBtn1) {
-//     themeToggleBtn1.addEventListener("click", () => {
-//         changeContactImg();
-//     });
-// }
+function changeContactImg() {
+  if (document.body.className.includes("dark")) {
+    contactImg.src = "/img/contact-dark.png";
+    contactImg.alt = "contact monkey";
+    contactImg.className = "contact-monkey";
+  } else {
+    contactImg.src = "/img/contact-light.png";
+    contactImg.alt = "contact monkey";
+    contactImg.className = "contact-monkey";
+  }
+}
+
+themeToggleBtn1.addEventListener("click", function () {
+    changeContactImg();
+});
+
+
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('hamburger--active');
+  navigation.classList.toggle('navigation--active');
+});
+
+document.querySelectorAll('.navigation__item').forEach((item) => {
+  item.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger--active');
+    navigation.classList.toggle('navigation--active');
+  });
+});
+
+window.window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,61 +1,31 @@
-// Deklaracje zmiennych
-var themeToggleBtn1 = document.querySelector("#theme-toggle");
-var contactImg = document.querySelector(".contact-monkey");
-var nav = document.getElementById('menu');
-var hamburger = document.querySelector('.hamburger');
-var navigation = document.querySelector('.navigation__list');
-
-// Funkcje odpowiadające za obrazki małp i ich kolor.
-function changeContactImg() {
-    if (document.body.className.includes("dark")) {
-        contactImg.src = "/img/contact-dark.png";
-        contactImg.alt = "contact monkey";
-        contactImg.className = "contact-monkey";
-    } else {
-        contactImg.src = "/img/contact-light.png";
-        contactImg.alt = "contact monkey";
-        contactImg.className = "contact-monkey";
-    }
-}
-
-// Funkcja obsługująca przesuwanie menu
-function toggleMenu() {
-    hamburger.classList.toggle('hamburger--active');
-    if (navigation) {
-        navigation.classList.toggle('navigation--active');
-    }
-    if (nav) {
-        nav.classList.toggle('menu--active');
-    }
-}
-
-// Nasłuchiwanie kliknięcia na przycisk hamburgera
-if (hamburger) {
-    hamburger.addEventListener('click', toggleMenu);
-}
-
-// Obsługa przesuwania menu w momencie scrollowania
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 0 && nav) {
-        nav.classList.add('sticky');
-    } else if (nav) {
-        nav.classList.remove('sticky');
-    }
-});
-
-// Nasłuchiwanie kliknięcia na elementy menu (aby schować menu po wybraniu opcji)
-document.querySelectorAll('.navigation__item').forEach((item) => {
-    item.addEventListener('click', () => {
-        toggleMenu();
-    });
-});
-
-// Nasłuchiwanie kliknięcia na przycisk zmiany motywu
-if (themeToggleBtn1) {
-    themeToggleBtn1.addEventListener("click", () => {
-        changeContactImg();
-    });
-}
+// // Deklaracje zmiennych
+// var themeToggleBtn = document.querySelector("#theme-toggle");
+// const element = document.querySelector(".profile_inner");
+// const heroImg = document.createElement("img");
+// element.appendChild(heroImg);
 
 
-console.log("laduje js")
+
+// // Nadanie atrybutów dla obrazka kontaktu
+
+
+// heroImg.classList.add("hero-img");
+
+// if (document.body.className.includes("dark")) {
+//   heroImg.src = "/img/dark.png";
+
+// } else {
+//   heroImg.src = "/img/light.png";
+// }
+
+// function changeHeroImg() {
+//   if (document.body.className.includes("dark")) {
+//     heroImg.src = "/img/dark.png";
+//   } else {
+//     heroImg.src = "/img/light.png";
+//   }
+// }
+
+// themeToggleBtn.addEventListener("click", function () {
+//   changeHeroImg();
+// });
